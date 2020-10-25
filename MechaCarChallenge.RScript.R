fb4944493ba6940e50dfcc2146920f42d059f4f4
Elisa-m <68163195+Elisa-m@users.noreply.github.com>
2020-10-25 09:41:09 -0700

    Add Deliverable 3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -19,3 +19,21 @@ total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI),Median=media
 # Create dataframe to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
 lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), Standard_Deviation=sd(PSI), .groups = 'keep')
 
+# T-test for all lots
+ttest_all <- t.test(Suspension_Coil_table$PSI, mu=mean(Suspension_Coil_table$PSI))
+ttest_all
+
+#T-test for Lot1
+population_lot1 = subset(Suspension_Coil_table, Manufacturing_Lot == "Lot1")
+ttest_lot1 <- t.test(population_lot1$PSI, mu=mean(population_lot1$PSI))
+ttest_lot1
+
+#T-test for Lot2
+population_lot2 = subset(Suspension_Coil_table, Manufacturing_Lot == "Lot2")
+ttest_lot2 <- t.test(population_lot2$PSI, mu=mean(population_lot2$PSI))
+ttest_lot2
+
+#T-test for Lot3
+population_lot3 = subset(Suspension_Coil_table, Manufacturing_Lot == "Lot3")
+ttest_lot3 <- t.test(population_lot3$PSI, mu=mean(population_lot3$PSI))
+ttest_lot3
